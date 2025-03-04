a3e71acc238b65fb5295f377b9721ef17eb76148
Ycaro Rodrigues <112430708+ycarossauro@users.noreply.github.com>
2025-03-04 16:32:50 -0300

    feat: create and instancing the viewModel

---
+++ app/src/main/java/com/example/mynewsapp/adapters/util/Resource.kt
@@ -0,0 +1,13 @@
+package com.example.mynewsapp.adapters.util
+
+/**
+ *  handle different states of data when making API calls
+ */
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Sucess<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
+}
---
+++ app/src/main/java/com/example/mynewsapp/repository/NewsRepository.kt
@@ -0,0 +1,7 @@
+package com.example.mynewsapp.repository
+
+import com.example.mynewsapp.db.ArticleDataBase
+
+class NewsRepository(
+    val db: ArticleDataBase
+)
