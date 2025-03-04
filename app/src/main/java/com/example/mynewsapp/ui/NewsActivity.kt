2791bab7029f6b42ef83a84bff711f445bdaf1b3
Ycaro Rodrigues <112430708+ycarossauro@users.noreply.github.com>
2025-03-04 13:01:22 -0300

    feat: create navigation in the activity and onviewCreate on SearchFragment

--- app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
@@ -2,10 +2,25 @@ package com.example.mynewsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mynewsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+        // Find the BottomNavigationView
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        // Find the NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        // Get the NavController from the NavHostFragment
+        val navController = navHostFragment.navController
+        // Find the NavHostFragment
+        bottomNavigationView.setupWithNavController(navController)
     }
-
 }
--- app/src/main/java/com/example/mynewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/mynewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,7 +1,14 @@
 package com.example.mynewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.mynewsapp.R
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
+
