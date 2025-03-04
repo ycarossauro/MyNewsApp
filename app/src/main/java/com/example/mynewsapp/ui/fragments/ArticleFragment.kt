a3f125c9d3572a225fe74dd7bf61220025df7bb8
Ycaro Rodrigues <112430708+ycarossauro@users.noreply.github.com>
2025-03-04 01:40:53 -0300

    feat: create ArticleFragment and the bottom_navigation_menu

---
+++ app/src/main/java/com/example/mynewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mynewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mynewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
